51ca7eebc2c221d0943a2368091d540123a3cde6
Максим Колосов <logan1.max@gmail.com>
2022-01-22 02:47:37 +0300

    2

--- ActorsCompare/Program.cs
+++ ActorsCompare/Program.cs
@@ -7,9 +7,9 @@ namespace ActorsCompare
         public int id;
         public string name;
 
-        Actor()
+        Actor(int _id)
         {
-
+            id = _id;
         }
     }
     class ActorsComparator
@@ -17,6 +17,21 @@ namespace ActorsCompare
         public Actor firstActor;
         public Actor secondActor;
 
+        private void getActor(int id)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                using (var request = new HttpRequestMessage(new HttpMethod("GET"), "https://kinopoiskapiunofficial.tech/api/v1/staff/" + id.ToString()))
+                {
+                    request.Headers.TryAddWithoutValidation("accept", "application/json");
+                    request.Headers.TryAddWithoutValidation("X-API-KEY", "23dd74b2-f381-4657-9433-4ea66638f27d");
+
+                    var response = await httpClient.SendAsync(request);
+
+                    Console.WriteLine();
+                }
+            }
+        }
         public ActorsComparator(Actor _firstActor, Actor _secondActor)
         {
             firstActor = _firstActor;
@@ -32,7 +47,7 @@ namespace ActorsCompare
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("https://kinopoiskapiunofficial.tech/api/v1/staff/7836" + "123");
 
         }
     }
