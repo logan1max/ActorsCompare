ed6987518eb0d5f980ba6135afb4966d5979dca2
Максим Колосов <logan1.max@gmail.com>
2022-01-22 00:19:14 +0300

    Добавьте файлы проекта.

---
+++ ActorsCompare/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ActorsCompare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
