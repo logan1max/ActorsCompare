8885cd8380e16f650c85ebd61a8adf73453dcff8
Максим Колосов <logan1.max@gmail.com>
2022-01-22 01:06:26 +0300

    1

--- ActorsCompare/Program.cs
+++ ActorsCompare/Program.cs
@@ -2,11 +2,38 @@
 
 namespace ActorsCompare
 {
+    class Actor
+    {
+        public int id;
+        public string name;
+
+        Actor()
+        {
+
+        }
+    }
+    class ActorsComparator
+    {
+        public Actor firstActor;
+        public Actor secondActor;
+
+        public ActorsComparator(Actor _firstActor, Actor _secondActor)
+        {
+            firstActor = _firstActor;
+            secondActor = _secondActor;
+        }
+        public void CompareActors()
+        {
+
+        }
+    }
+    
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+
         }
     }
 }
