f193afa29dd6caf8f4e2bcdd8ecd7661cbe7d704
Максим Колосов <logan1.max@gmail.com>
2022-01-24 12:07:06 +0300

    к

--- ActorsCompare/Program.cs
+++ ActorsCompare/Program.cs
@@ -4,7 +4,7 @@ using System.Net;
 using System.Net.Http;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-//using System.Text.Json;
+using System.Linq;
 using System.Collections.Generic;
 
 namespace ActorsCompare
@@ -37,8 +37,11 @@ namespace ActorsCompare
 
     class ActorsComparator
     {
+        private const int V = 2;
         public Actor firstActor;
-        public Actor secondActor;       
+        public Actor secondActor;
+        public int[] firstMovies;
+        public int[] secondMovies;
 
         public ActorsComparator(Actor _firstActor, Actor _secondActor)
         {
@@ -69,29 +72,100 @@ namespace ActorsCompare
             return resultJSON;
         }
 
-        public void ParseActor(string firstActorJSON)
+        public List<Movie> ParseActor(string actJSON)
         {
-            dynamic data = JObject.Parse(firstActorJSON);
+            dynamic data = JObject.Parse(actJSON);
             
-
             Console.WriteLine(data);
-            dynamic data1 = JsonConvert.DeserializeObject(firstActorJSON);
+            dynamic data1 = JsonConvert.DeserializeObject(actJSON);
             Console.WriteLine(data1);
             
-            ActorJSON actorJSON = JsonConvert.DeserializeObject<ActorJSON>(firstActorJSON);
+            ActorJSON actorJSON = JsonConvert.DeserializeObject<ActorJSON>(actJSON);
+
             Console.WriteLine(actorJSON.nameRu);
             foreach(var item in actorJSON.films)
             {
                 if (item.professionKey == "ACTOR")
                 {
+
                     Console.WriteLine(item.filmId + " " + item.nameRu + " " + item.description + " " + item.professionKey + " " + item.rating);
+                }
+            }
+
+            return actorJSON.films;
+        }
+
+        public string[,] MovieListToArray(List<Movie> movie)
+        {
+            string[,] str = new string[movie.Count,2];
+            int i = 0;
+
+            foreach (var item in movie)
+            {
+                str[i, 0] = item.filmId.ToString();
+                str[i, 1] = item.nameRu;
+                i++;
+            }
+
+            return str;
+        }
+
+        public void CompareMovieList(List<Movie> firstList, List<Movie> secondList)
+        {
+            foreach (var i in firstList)
+            {
+                foreach (var j in secondList)
+                {
+                    if (i.filmId == j.filmId)
+                    {
+
+                    }
                 }
             }
         }
 
         public void CompareActors()
         {
+            string jsonFirst = GetActor(firstActor.id);
+            string jsonSecond = GetActor(secondActor.id);
 
+            List<Movie> firstList = ParseActor(jsonFirst);
+            List<Movie> secondList = ParseActor(jsonSecond);
+
+            List<string> res = new List<string>();
+
+            foreach(Movie m1 in firstList)
+            {
+                foreach(Movie m2 in secondList)
+                {
+                    if ((m1.filmId == m2.filmId) && !res.Contains(m1.filmId.ToString()) && m1.rating!=null && m2.rating!=null)
+                    {
+                        res.Add(m1.filmId.ToString());
+                        Console.WriteLine(m1.filmId + " " + m1.nameRu + " " + m1.rating);
+                    }
+                }
+            }
+            Console.WriteLine("------------------------------");
+            foreach(string s in res)
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine("------------------------------");
+
+
+            string[,] firstArray = MovieListToArray(firstList);
+            string[,] secondArray = MovieListToArray(secondList);
+
+            for (int i = 0; i < firstList.Count; i++)
+            {
+                for (int j = 0; j < secondList.Count; j++)
+                {
+                    if(firstArray[i,0] == secondArray[j,0])
+                    {
+                        Console.WriteLine("id: " + firstArray[i,0] + " name: " + firstArray[i,1] );
+                    }
+                }
+            }
         }
     }
     
@@ -104,12 +178,15 @@ namespace ActorsCompare
 
             var comparator = new ActorsComparator(firstActor, secondActor);
 
+            comparator.CompareActors();
+            /*
             string jsonA = comparator.GetActor(firstActor.id);
             string jsonB = comparator.GetActor(secondActor.id);
             Console.WriteLine("----------------------------------------------");
             comparator.ParseActor(jsonA);
             comparator.ParseActor(jsonB);
             // comparator.GetActor(secondActor.id);
+            */
         }
     }
 }
