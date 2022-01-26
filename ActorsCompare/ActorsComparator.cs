2a6b88fb0ac9b426edbd67fb0593ad5931261ee2
Максим Колосов <logan1.max@gmail.com>
2022-01-26 22:06:29 +0300

    add model for common films

--- ActorsCompare/ActorsComparator.cs
+++ ActorsCompare/ActorsComparator.cs
@@ -10,11 +10,8 @@ namespace ActorsCompare
 {
     class ActorsComparator
     {
-        private const int V = 2;
         public Actor firstActor;
         public Actor secondActor;
-        public int[] firstMovies;
-        public int[] secondMovies;
 
         public ActorsComparator(Actor _firstActor, Actor _secondActor)
         {
@@ -40,19 +37,12 @@ namespace ActorsCompare
             }
 
             Console.WriteLine(httpResponse.StatusCode);
-            // Console.WriteLine(resultJSON);
 
             return resultJSON;
         }
 
         public List<Movie> ParseActor(string actJSON)
         {
-            dynamic data = JObject.Parse(actJSON);
-
-            Console.WriteLine(data);
-            dynamic data1 = JsonConvert.DeserializeObject(actJSON);
-            Console.WriteLine(data1);
-
             ActorJSON actorJSON = JsonConvert.DeserializeObject<ActorJSON>(actJSON);
 
             Console.WriteLine(actorJSON.nameRu);
@@ -60,7 +50,6 @@ namespace ActorsCompare
             {
                 if (item.professionKey == "ACTOR")
                 {
-
                     Console.WriteLine(item.filmId + " " + item.nameRu + " " + item.description + " " + item.professionKey + " " + item.rating);
                 }
             }
@@ -68,33 +57,37 @@ namespace ActorsCompare
             return actorJSON.films;
         }
 
-        public string[,] MovieListToArray(List<Movie> movie)
+        //public string[,] MovieListToArray(List<Movie> movie)
+        //{
+        //    string[,] str = new string[movie.Count, 2];
+        //    int i = 0;
+
+        //    foreach (var item in movie)
+        //    {
+        //        str[i, 0] = item.filmId.ToString();
+        //        str[i, 1] = item.nameRu;
+        //        i++;
+        //    }
+
+        //    return str;
+        //}
+
+        public List<CommonFilm> CompareMovieList(List<Movie> firstList, List<Movie> secondList)
         {
-            string[,] str = new string[movie.Count, 2];
-            int i = 0;
+            List<CommonFilm> res = new List<CommonFilm>();
 
-            foreach (var item in movie)
-            {
-                str[i, 0] = item.filmId.ToString();
-                str[i, 1] = item.nameRu;
-                i++;
-            }
-
-            return str;
-        }
-
-        public void CompareMovieList(List<Movie> firstList, List<Movie> secondList)
-        {
             foreach (var i in firstList)
             {
                 foreach (var j in secondList)
                 {
                     if (i.filmId == j.filmId)
                     {
-
+                        CommonFilm commonFilm = new CommonFilm(i.filmId, firstActor.id, secondActor.id, i.description, j.description, i.rating);
+                        res.Add(commonFilm);
                     }
                 }
             }
+            return res;
         }
 
         public void CompareActors()
@@ -105,40 +98,46 @@ namespace ActorsCompare
             List<Movie> firstList = ParseActor(jsonFirst);
             List<Movie> secondList = ParseActor(jsonSecond);
 
-            List<string> res = new List<string>();
+            List<CommonFilm> res = CompareMovieList(firstList, secondList);
 
-            foreach (Movie m1 in firstList)
+            foreach (var i in res)
             {
-                foreach (Movie m2 in secondList)
-                {
-                    if ((m1.filmId == m2.filmId) && !res.Contains(m1.filmId.ToString()) && m1.rating != null)
-                    {
-                        res.Add(m1.filmId.ToString());
-                        Console.WriteLine(m1.filmId + " " + m1.nameRu + " " + m1.rating);
-                    }
-                }
+                Console.WriteLine("id: " + i.filmId + " id1: " + i.firstId + " role1: " + i.firstHero + " id2: " + i.secondId + " role2: " + i.secondHero + " rate: " + i.rating);
             }
-            Console.WriteLine("------------------------------");
-            foreach (string s in res)
-            {
-                Console.WriteLine(s);
-            }
-            Console.WriteLine("------------------------------");
+            //List<string> res = new List<string>();
+
+            //foreach (Movie m1 in firstList)
+            //{
+            //    foreach (Movie m2 in secondList)
+            //    {
+            //        if ((m1.filmId == m2.filmId) && !res.Contains(m1.filmId.ToString()) && m1.rating != null)
+            //        {
+            //            res.Add(m1.filmId.ToString());
+            //            Console.WriteLine(m1.filmId + " " + m1.nameRu + " " + m1.rating);
+            //        }
+            //    }
+            //}
+            //Console.WriteLine("------------------------------");
+            //foreach (string s in res)
+            //{
+            //    Console.WriteLine(s);
+            //}
+            //Console.WriteLine("------------------------------");
 
 
-            string[,] firstArray = MovieListToArray(firstList);
-            string[,] secondArray = MovieListToArray(secondList);
+            //string[,] firstArray = MovieListToArray(firstList);
+            //string[,] secondArray = MovieListToArray(secondList);
 
-            for (int i = 0; i < firstList.Count; i++)
-            {
-                for (int j = 0; j < secondList.Count; j++)
-                {
-                    if (firstArray[i, 0] == secondArray[j, 0])
-                    {
-                        //     Console.WriteLine("id: " + firstArray[i,0] + " name: " + firstArray[i,1] );
-                    }
-                }
-            }
+            //for (int i = 0; i < firstList.Count; i++)
+            //{
+            //    for (int j = 0; j < secondList.Count; j++)
+            //    {
+            //        if (firstArray[i, 0] == secondArray[j, 0])
+            //        {
+            //            //     Console.WriteLine("id: " + firstArray[i,0] + " name: " + firstArray[i,1] );
+            //        }
+            //    }
+            //}
         }
     }
 }
---
+++ ActorsCompare/CommonFilm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ActorsCompare
+{
+    public class CommonFilm
+    {
+        public int filmId;
+        public int firstId;
+        public int secondId;
+        public string firstHero;
+        public string secondHero;
+        public string rating;
+        public int year;
+
+        public CommonFilm(int _filmId, int _firstId, int _secondId, string _firstHero, string _secondHero, string _rating)
+        {
+            filmId = _filmId;
+            firstId = _firstId;
+            secondId = _secondId;
+            firstHero = _firstHero;
+            secondHero = _secondHero;
+            rating = _rating;
+        }
+
+        
+    }
+}
