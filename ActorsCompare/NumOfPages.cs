63a90f795bad00911d6be652f63c627f52dde02c
Максим Колосов <logan1.max@gmail.com>
2022-02-09 04:51:12 +0300

    get started to remove if

---
+++ ActorsCompare/NumOfPages.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ActorsCompare
+{
+    public abstract class NumOfPages
+    {
+        public abstract int GetNumOfPages();
+    }
+
+    public class ZeroPages : NumOfPages
+    {
+        public override int GetNumOfPages(int num < 0 ? 0:1)
+        {
+
+        }
+    }
+}
--- ActorsCompare/SearchModule.cs
+++ ActorsCompare/SearchModule.cs
@@ -11,62 +11,66 @@ namespace ActorsCompare
 
         public int ByName(string name)
         {
-         //   Console.WriteLine("Поиск персон по имени...");
-         //   Console.WriteLine("Введите имя для поиска: ");
-
-      //      string searchName = Console.ReadLine();// никакого считывания здесь, этот метод уже должен принимать строку
-
-            ActorSearchModel actorSearchModel = api.ParseActorListByName(name, 1);
-
-            int pages = 0;
-            if (actorSearchModel.total == 0)//это костыль из ифов, подумай как от него избавиться (почитай на тему избавления от ифов спомощью ооп)
+            if (!string.IsNullOrEmpty(name))
             {
-                Console.WriteLine("Персоны не найдены...");
-                pages = 0;
-            }
-            else if (actorSearchModel.total <= 50)
-            {
-                Console.WriteLine("Найдено " + actorSearchModel.total + " персон...");
-                pages = 1;
-            }
-            else if (actorSearchModel.total < 100)
-            {
-                Console.WriteLine("Найдено " + actorSearchModel.total + " персон...");
-                pages = 2;
-            }
-            else if (actorSearchModel.total > 100)
-            {
-                Console.WriteLine("Найдено более 100 персон...");
-                pages = 2;
-            }
+                //      string searchName = Console.ReadLine();// никакого считывания здесь, этот метод уже должен принимать строку
 
-            int numItems = 1;
+                ActorSearchModel actorSearchModel = api.ParseActorListByName(name, 1);
 
-            if (pages > 0)
-            {
-                foreach (var i in actorSearchModel.items)
+                int pages = 0;
+                if (actorSearchModel.total == 0)//это костыль из ифов, подумай как от него избавиться (почитай на тему избавления от ифов спомощью ооп)
                 {
-                    Console.WriteLine("n: "+ numItems+" id: " + i.kinopoiskId + " nameRu: " + i.nameRu + " nameEn: " + i.nameEn + " sex: " + i.sex);
-                    numItems++;
+                    Console.WriteLine("Персоны не найдены...");
+                    pages = 0;
                 }
-            }
-
-            if (pages == 2)// не понимаю зачем?
-            {
-                ActorSearchModel actorSearchModel1 = api.ParseActorListByName(name, 2);
-                
-                foreach (var i in actorSearchModel1.items)
+                else if (actorSearchModel.total <= 50)
                 {
-                    actorSearchModel.items.Add(i);
-                    Console.WriteLine("n: " + numItems + " id: " + i.kinopoiskId + " nameRu: " + i.nameRu + " nameEn: " + i.nameEn + " sex: " + i.sex);
-                    numItems++;
+                    Console.WriteLine("Найдено " + actorSearchModel.total + " персон...");
+                    pages = 1;
                 }
+                else if (actorSearchModel.total < 100)
+                {
+                    Console.WriteLine("Найдено " + actorSearchModel.total + " персон...");
+                    pages = 2;
+                }
+                else if (actorSearchModel.total > 100)
+                {
+                    Console.WriteLine("Найдено более 100 персон...");
+                    pages = 2;
+                }
+
+                int numItems = 1;
+
+                if (pages > 0)
+                {
+                    foreach (var i in actorSearchModel.items)
+                    {
+                        Console.WriteLine("n: " + numItems + " id: " + i.kinopoiskId + " nameRu: " + i.nameRu + " nameEn: " + i.nameEn + " sex: " + i.sex);
+                        numItems++;
+                    }
+                }
+
+                if (pages == 2)// не понимаю зачем?
+                {
+                    ActorSearchModel actorSearchModel1 = api.ParseActorListByName(name, 2);
+
+                    foreach (var i in actorSearchModel1.items)
+                    {
+                        actorSearchModel.items.Add(i);
+                        Console.WriteLine("n: " + numItems + " id: " + i.kinopoiskId + " nameRu: " + i.nameRu + " nameEn: " + i.nameEn + " sex: " + i.sex);
+                        numItems++;
+                    }
+                }
+
+                Console.WriteLine("Введите номер: ");
+                int num = Convert.ToInt32(Console.ReadLine());//подумай как от этого избавиться
+
+                return actorSearchModel.items[num - 1].kinopoiskId;
             }
-
-            Console.WriteLine("Введите номер: ");
-            int num = Convert.ToInt32(Console.ReadLine());//подумай как от этого избавиться
-
-            return actorSearchModel.items[num-1].kinopoiskId;
+            else
+            {
+                throw new Exception("Wrong name");
+            }
         }
     }
 }
